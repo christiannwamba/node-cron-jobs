62fec11088cda8ffb6b1460d8e9beb34c4215c72
Keith Mock <keithmock@Keiths-MacBook-Pro.local>
2018-12-03 20:29:09 -0800

    cron and mailer

--- data_dump.sql
+++ data_dump.sql
@@ -1,5 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE users(name text);
-INSERT INTO "users" VALUES('Chris');
+CREATE TABLE wheresMyBike(id integer);
+INSERT INTO "id" VALUES('0');
 COMMIT;
--- index.js
+++ index.js
@@ -6,54 +6,96 @@ let nodemailer = require("nodemailer");
 
 app = express();
 
-// deleting error.log files from server on the 21st of every month
-cron.schedule("* * 21 * *", function() {
-  console.log("---------------------");
-  console.log("Running Cron Job");
-  fs.unlink("./error.log", err => {
-    if (err) throw err;
-    console.log("Error file succesfully deleted");
-  });
-});
+// query parking status from SQlite
+const getParkingStatus = () => {
+    //replace this with a SQlite query
+    return 2;
+};
 
-// sending emails at periodic intervals
+// tzone configs
+const zonesNearHome = [
+    {
+        id: 1,
+        desc: 'N. Page',
+        fequency: 'Tuesdays at 12pm',
+        dayBeforeReminder: '* 19 * * Monday',  // every Monday at 7pm
+        morningOfReminder: '* 7 * * Tuesday'  // every Tuesday at 7am
+    }, {
+        id: 2,
+        desc: 'S. Page',
+        fequency: 'Thursdays at 8am',
+        dayBeforeReminder: '* 19 * * Wednesday',  // every Wednesday at 7pm
+        morningOfReminder: '* 7 * * Thursday',  // every Thursday at 7am
+    }, {
+        id: 3,
+        desc: 'W. Pierce',
+        fequency: 'Fridays at 9am',
+        dayBeforeReminder: '* 19 * * Thursday',  // every Thursday at 7pm
+        morningOfReminder: '* 7 * * Friday'  // every Friday at 7am
+    }, {
+        id: 4,
+        desc: 'E. Pierce',
+        fequency: 'Wednesdays at 9am',
+        dayBeforeReminder: '* 19 * * Tuesday',  // every Tuesday at 7pm
+        morningOfReminder: '* 7 * * Wednesday'  // every Wednesday at 7am
+    }
+];
+const zonesNearHomeTEST = [
+    {
+        id: 1,
+        desc: 'N. Page',
+        fequency: 'Tuesdays at 12pm',
+        dayBeforeReminder: '*/30 * * * * *',  // every Monday at 7pm
+        morningOfReminder: '*/31 * * * * *'  // every Tuesday at 7am
+    }, {
+        id: 2,
+        desc: 'S. Page',
+        fequency: 'Thursdays at 8am',
+        dayBeforeReminder: '*/5 * * * * *',  // every Wednesday at 7pm
+        morningOfReminder: '*/6 * * * * *',  // every Thursday at 7am
+    }
+];
 
 // create mail transporter
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "COMPANYEMAIL@gmail.com",
-    pass: "COMPANYPASS"
+    user: "parking.budddy@gmail.com",
+    pass: "noMOREt1ick3ts"
   }
 });
 
-cron.schedule("* * * * Wednesday", function() {
-  console.log("---------------------");
-  console.log("Running Cron Job");
-  let mailOptions = {
-    from: "COMPANYEMAIL@gmail.com",
-    to: "RECEPIENTEMAIL@gmail.com",
-    subject: `Not a GDPR update ;)`,
-    text: `Hi there, this email was automatically sent by us`
-  };
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      throw error;
-    } else {
-      console.log("Email successfully sent!");
-    }
-  });
-});
+// setup cron jobs for configured zones
+for(id in zonesNearHomeTEST) {
+    const zone = zonesNearHomeTEST[id];
+    cron.schedule(zone.dayBeforeReminder, function() {
+        if(getParkingStatus() === zone.id) {
+            // send email for day before reminder
+            // light up warning lights
+        } 
+    });
+    cron.schedule(zone.morningOfReminder, function() {
+        if(getParkingStatus() === zone.id) {
+            // send email for morning of reminder
+            // light up warning lights
+        } 
+    });
+}
 
-// To backup a database
-cron.schedule("59 23 * * *", function() {
-  console.log("---------------------");
-  console.log("Running Cron Job");
-  if (shell.exec("sqlite3 database.sqlite  .dump > data_dump.sql").code !== 0) {
-    shell.exit(1);
-  } else {
-    shell.echo("Database backup complete");
-  }
-});
+app.listen("2319");
 
-app.listen("3128");
+// copy this into the action, example
+
+// transporter.sendMail({
+//     from: "parking.budddy@gmail.com",
+//     to: "parking.budddy@gmail.com",
+//     subject: `Testing cron job`,
+//     text: `Hi there, this email was automatically sent by PB ;)`
+//   }, function(error, info) {
+//   if (error) {
+//     throw error;
+//   } else {
+//     console.log("Email sent");
+//   }
+// });
+
