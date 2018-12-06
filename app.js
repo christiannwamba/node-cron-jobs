0648b2e085cf94a760c6ebf91f0e4e821774627c
Keith Mock <keithmock@Keiths-MacBook-Pro.local>
2018-12-05 21:32:31 -0800

    update

--- app.js
+++ app.js
@@ -83,19 +83,19 @@ fetch('http://localhost:3000/test_zones')
 
 // setup warning light/trigger
 var warningLED = new Gpio(5, 'out');
+var blinkInterval = setInterval(warningLED, 200); //run the blinkLED function every 250ms
+
 var triggerWarning = () => {
-    var blinkInterval = setInterval(warningLED, 200); //run the blinkLED function every 250ms
     if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     LED.writeSync(1); //set pin state to 1 (turn LED on)
     } else {
     LED.writeSync(0); //set pin state to 0 (turn LED off)
     }
-    function endBlink() { //function to stop blinking
+    setTimeout(() => { //function to stop blinking
         clearInterval(blinkInterval); // Stop blink intervals
         LED.writeSync(0); // Turn LED off
         LED.unexport(); // Unexport GPIO to free resources
-    }
-    setTimeout(endBlink, 2000); //stop blinking after 5 seconds
+    }, 2000); //stop blinking after 5 seconds
 };
 
 triggerWarning();
